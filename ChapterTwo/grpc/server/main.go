8cdff08fa8100f2ef8d8eda09082780f0d83f956
codeLambQ <codeLamb@163.com>
2025-08-26 17:05:32 +0800

    grpc server 端的简单使用

--- ChapterTwo/grpc/server/main.go
+++ ChapterTwo/grpc/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
+	"log"
 	"net"
 )
 
@@ -20,9 +21,16 @@ func (l *LessonServiceImpl) GetLesson(context.Context, *LessonId) (*Lesson, erro
 }
 func main() {
 	lessonServiceClient := LessonServiceImpl{}
+	// 注册一个端口
 	listen, _ := net.Listen("tcp", ":8080")
+	// 创建一个 grpc 服务
 	grpcServer := grpc.NewServer()
+	// 注册服务端的 grpc 服务
 	RegisterLessonServiceServer(grpcServer, &lessonServiceClient)
+	// Serve 会通过监听器 lis 接收传入的连接，为每个连接创建一个新的 ServerTransport 和服务 goroutine
+	err := grpcServer.Serve(listen)
 
-	grpcServer.Serve(listen)
+	if err != nil {
+		log.Fatalf("grpcServer.Serve err: %v", err)
+	}
 }
