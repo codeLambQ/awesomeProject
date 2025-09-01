43385080443b1ad1cd16369b7226c69f0e77670a
codeLambQ <codeLamb@163.com>
2025-09-01 15:34:31 +0800

    ent 框架操作数据库

--- ChapterTwo/kratos-demo/internal/data/data.go
+++ ChapterTwo/kratos-demo/internal/data/data.go
@@ -1,10 +1,14 @@
 package data
 
 import (
-	"kratos-demo/internal/conf"
-
+	"context"
+	"entgo.io/ent/dialect/sql"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-redis/redis/v8"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/wire"
+	"kratos-demo/internal/conf"
+	"kratos-demo/internal/data/ent"
 )
 
 // ProviderSet is data providers.
@@ -13,12 +17,41 @@ var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
 // Data .
 type Data struct {
 	// TODO wrapped database client
+	mysql *ent.Client
+	redis *redis.Client
 }
 
 // NewData .
 func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
-	cleanup := func() {
-		log.NewHelper(logger).Info("closing the data resources")
+	log := log.NewHelper(logger)
+	driver, err := sql.Open(c.Database.Driver, c.Database.Source)
+	if err != nil {
+		log.Errorf("open db error, %s", err)
 	}
-	return &Data{}, cleanup, nil
+	mysqlClient := ent.NewClient(ent.Driver(driver))
+
+	if err := mysqlClient.Schema.Create(context.Background()); err != nil {
+		log.Errorf("failed creating schema resources: %v", err)
+		return nil, nil, err
+	}
+
+	mysqlClient.Article.Create().SetID(1).SetTitle("mysql").SetContext("sdada").Save(context.Background())
+	article, _ := mysqlClient.Article.Get(context.Background(), 1)
+	println(article.String())
+
+	redisClient := redis.NewClient(&redis.Options{})
+
+	d := &Data{
+		mysql: mysqlClient,
+		redis: redisClient,
+	}
+	return d, func() {
+		log.Info("message", "closing the data resources")
+		if err := mysqlClient.Close(); err != nil {
+			log.Error(err)
+		}
+		if err := redisClient.Close(); err != nil {
+			log.Error(err)
+		}
+	}, nil
 }
